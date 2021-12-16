f249bbb652d30e84eadce0ba61f0688eca43d64a
Thai Dinh Le <Thai-Dinh@hotmail.com>
2021-12-16 14:15:57 +0100

    Python3 solution for two characters problem

---
+++ python3/algorithms/two_characters.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from itertools import combinations
+
+def alternate(s):
+	"""Determine the longest string possible containing only two alternating letters
+
+	Keyword arguments:
+	s : string
+	Return:
+	longuest : Longuest string possible containing only two alternating letters
+	"""
+
+	keys = list(Counter(list(s)).keys())
+	pairs = combinations(keys, 2)
+	longest = 0
+
+	for pair in pairs:
+		string = s[:]
+		string = ''.join([c for c in string if c in list(pair)])
+		
+		current = 1
+		for i in range(len(string)-1):
+			if string[i] == string[i+1]:
+				current = 0
+				break
+			else:
+				current += 1
+
+		longest = max(longest, current)
+
+	return longest
